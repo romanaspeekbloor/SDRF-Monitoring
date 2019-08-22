9dc301e0f67c746204f5fcb26e8cef406aefaeae
n3op2 <n3op21@gmail.com>
2019-08-22 15:06:47 +0100

    Updated client.

--- sdrf_client/client.js
+++ sdrf_client/client.js
@@ -1,14 +1,10 @@
 // Imports/Dependencies
 
-// axios - for making http request (post/get/etc)
-const axios = require('axios')
-// for receiving output data for example rtl_power ... | node server.js
-const stdin = process.openStdin()
 
 // Event handlers, executes function on 'data' event
-stdin.on('data', (data) =>{
-  console.log('data',data.toString())
-  const convertedData = data.toString()
+stdin.on('data', (raw) =>{
+  console.log('data',raw.toString())
+  const convertedData = raw.toString()
   // http headers
   const headers = {
     headers: {
@@ -16,8 +12,13 @@ stdin.on('data', (data) =>{
     }
   }
 
+  const data = {
+    name: 'RX1_BLACK_PI',
+    iq: raw
+  };
+
   // post request using axios to the server
-  axios.post("http://192.168.10.242:3010/rtldata", { data: convertedData }, headers)
+  axios.post("http://192.168.10.242:3010/rtldata", data, headers)
   .then(function (response) {
     console.log('res: ', response.data);
   })
