b03f8800c10629090bbfeb223ba6519602e2f41e
n3op2 <n3op2@n3op2.com>
2019-08-31 14:52:33 +0100

    Saving before i screwed something.

--- sdrf_client/client.js
+++ sdrf_client/client.js
@@ -1,27 +1,109 @@
 #!/usr/bin/env node
 // Imports/Dependencies
 const stdin = process.openStdin();
-const tsClient = require('timesync');
+const timesync = require('timesync');
 const axios = require('axios');
 const exec = require('child_process').exec;
+const requirejs = require('requirejs');
 
-const rtlPowerCMD = 'rtl_power -f 153084000:153304000:0.8k -g 35 -1'; 
+const id = 'asdasd';
 
-const ts = tsClient({
+require(['./lib/peer-min.js'], function(1, 'asd') {
+  console.log('a: ', a);
+});
+
+
+const rtlPowerCMD = 'sudo rtl_power -f 100M:100.3M:0.3k -g 35 -1'; 
+//cdn('https://cdnjs.cloudflare.com/ajax/libs/peerjs/0.3.14/peer.min.js', '$')
+//  .then($ => $('body').text('loaded'));
+//
+//
+const ts = timesync.create({
   interval: 10000,
-  peers
+  peers: []
 });
 
 let doSampling = true;
 
-ts.on('sync', (state) => {
-  console.log('state: ', state);
-  exec(rtlPowerCMD, (err, stdout, stderr) => {
-    if (err) console.log('ERROR: ', err);
-    console.log('out: ', stdout);
+// EXECUTE RTL_POWER  
+// ===============================================================
+const getSDRData = (cmd) => new Promise((resolve) => {
+  exec(cmd, (err, stdout) => {
+    if (err) return resolve(err); 
+    console.log(typeof stdout);
+    resolve(stdout);
   });
 });
 
+// ON TIME SYNC
+// ===============================================================
+ts.on('sync', async (state) => {
+  console.log('onSync state: ', state);
+  if (state === 'start') {
+    ts.options.peers = openConnections();
+    console.log('syncing with peers: ', ts.options.peers); 
+    if (ts.options.peers.length) {
+      console.log('syncing with: ', ts.options.peers);
+    }
+    /*
+    const data = await getSDRData(rtlPowerCMD);
+    if (typeof data === 'string') {
+      const freqs = data.split(',').splice(6);
+      console.log('freqs: ', freqs.length);
+    }
+    */
+  }
+});
+
+ts.on('change', (offset) => {
+  console.log('offSet, change: ', offset); 
+});
+
+ts.send = (id, data, timeout) => {
+  console.log('send: ', id, data, timeout);
+};
+
+// commenting out,
+//const peer = new Peer('NAME', {key: 'lwjd5qra8257b9', debug: 1}, 'asd');
+//var id = 'asd'
+//var peer = new Peer(id, {key: 'lwjd5qra8257b9', debug: 1});
+
+peer.on('open', connectToPeers);
+peer.on('connection', setupConnection);
+
+const openConnections = () => {
+    return Object.keys(peer.connections).filter(function (id) {
+      return peer.connections[id].some(function (conn) {
+        return conn.open;
+      });
+    });
+  }
+
+const connectToPeers = () => {
+    peers.filter(function (id) {
+      return peer.connections[id] === undefined;
+    }).forEach(function (id) {
+      const conn = peer.connect(id);
+      console.log('connecting with ' + id + '...');
+      setupConnection(conn);
+  });
+}
+
+const setupConnection = (conn) => {
+  conn.on('open', function () {
+    console.log('connected with ' + conn.peer);
+  })
+  .on('data', function(data) {
+    console.log('receive', conn.peer, data);
+    ts.receive(conn.peer, data);
+  })
+  .on('close', function () {
+    console.log('disconnected from ' + conn.peer);
+  })
+  .on('error', function (err) {
+    console.log('Error', err);
+  });
+}
 
 // Event handlers, executes function on 'data' event
 /*
